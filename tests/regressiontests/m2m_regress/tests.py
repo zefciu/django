zefciu/django

commit 4ff345081b7bfb09fc3ef7f65339d22cb944eb85

[1.2.X] Fixed #14456 -- converted inline_formsets tests from doctests to unittests.  We have always been at war with doctests.  Thanks to prestontimmons for the patch.  Backport of [14212].
git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14213 bcc190cf-cafb-0310-a4f2-bffc1f526a37

tests/regressiontests/inline_formsets/tests.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 from django.forms.models import inlineformset_factory
 from regressiontests.inline_formsets.models import Poet, Poem, School, Parent, Child
 
+
 class DeletionTests(TestCase):
+
     def test_deletion(self):
         PoemFormSet = inlineformset_factory(Poet, Poem, can_delete=True)
         poet = Poet.objects.create(name='test')
@@ -103,3 +105,51 @@ class DeletionTests(TestCase):
             obj.save()
         self.assertEqual(school.child_set.count(), 1)
 
+
+class InlineFormsetFactoryTest(TestCase):
+    def test_inline_formset_factory(self):
+        """
+        These should both work without a problem.
+        """
+        inlineformset_factory(Parent, Child, fk_name='mother')
+        inlineformset_factory(Parent, Child, fk_name='father')
+
+    def test_exception_on_unspecified_foreign_key(self):
+        """
+        Child has two ForeignKeys to Parent, so if we don't specify which one
+        to use for the inline formset, we should get an exception.
+        """
+        self.assertRaisesRegexp(Exception,
+            "<class 'regressiontests.inline_formsets.models.Child'> has more than 1 ForeignKey to <class 'regressiontests.inline_formsets.models.Parent'>",
+            inlineformset_factory, Parent, Child
+        )
+
+    def test_fk_name_not_foreign_key_field_from_child(self):
+        """
+        If we specify fk_name, but it isn't a ForeignKey from the child model
+        to the parent model, we should get an exception.
+        """
+        self.assertRaises(Exception,
+            "fk_name 'school' is not a ForeignKey to <class 'regressiontests.inline_formsets.models.Parent'>",
+            inlineformset_factory, Parent, Child, fk_name='school'
+        )
+
+    def test_non_foreign_key_field(self):
+        """
+        If the field specified in fk_name is not a ForeignKey, we should get an
+        exception.
+        """
+        self.assertRaisesRegexp(Exception,
+            "<class 'regressiontests.inline_formsets.models.Child'> has no field named 'test'",
+            inlineformset_factory, Parent, Child, fk_name='test'
+        )
+
+    def test_any_iterable_allowed_as_argument_to_exclude(self):
+        # Regression test for #9171.
+        inlineformset_factory(
+            Parent, Child, exclude=['school'], fk_name='mother'
+        )
+
+        inlineformset_factory(
+            Parent, Child, exclude=('school',), fk_name='mother'
+        )

tests/regressiontests/inline_formsets/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.db import models
 
+
 class School(models.Model):
     name = models.CharField(max_length=100)
 
@@ -25,46 +26,3 @@ class Poem(models.Model):
 
     def __unicode__(self):
         return self.name
-
-__test__ = {'API_TESTS': """
-
->>> from django.forms.models import inlineformset_factory
-
-
-Child has two ForeignKeys to Parent, so if we don't specify which one to use
-for the inline formset, we should get an exception.
-
->>> ifs = inlineformset_factory(Parent, Child)
-Traceback (most recent call last):
-    ...
-Exception: <class 'regressiontests.inline_formsets.models.Child'> has more than 1 ForeignKey to <class 'regressiontests.inline_formsets.models.Parent'>
-
-
-These two should both work without a problem.
-
->>> ifs = inlineformset_factory(Parent, Child, fk_name='mother')
->>> ifs = inlineformset_factory(Parent, Child, fk_name='father')
-
-
-If we specify fk_name, but it isn't a ForeignKey from the child model to the
-parent model, we should get an exception.
-
->>> ifs = inlineformset_factory(Parent, Child, fk_name='school')
-Traceback (most recent call last):
-    ...
-Exception: fk_name 'school' is not a ForeignKey to <class 'regressiontests.inline_formsets.models.Parent'>
-
-
-If the field specified in fk_name is not a ForeignKey, we should get an
-exception.
-
->>> ifs = inlineformset_factory(Parent, Child, fk_name='test')
-Traceback (most recent call last):
-    ...
-Exception: <class 'regressiontests.inline_formsets.models.Child'> has no field named 'test'
-
-
-# Regression test for #9171.
->>> ifs = inlineformset_factory(Parent, Child, exclude=('school',), fk_name='mother')
-"""
-}

commit 2a94afff18c72e57b4c3e54d096403627c502598

[1.2.X] Fixed a few other backporting-related bugs introduced in r14213.
git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14247 bcc190cf-cafb-0310-a4f2-bffc1f526a37

tests/regressiontests/inline_formsets/tests.py
@@ -107,6 +107,13 @@ class DeletionTests(TestCase):
 
 
 class InlineFormsetFactoryTest(TestCase):
+    def assertRaisesErrorWithMessage(self, error, message, callable, *args, **kwargs):
+        self.assertRaises(error, callable, *args, **kwargs)
+        try:
+            callable(*args, **kwargs)
+        except error, e:
+            self.assertEqual(message, str(e))
+
     def test_inline_formset_factory(self):
         """
         These should both work without a problem.
@@ -119,7 +126,7 @@ class InlineFormsetFactoryTest(TestCase):
         Child has two ForeignKeys to Parent, so if we don't specify which one
         to use for the inline formset, we should get an exception.
         """
-        self.assertRaisesRegexp(Exception,
+        self.assertRaisesErrorWithMessage(Exception,
             "<class 'regressiontests.inline_formsets.models.Child'> has more than 1 ForeignKey to <class 'regressiontests.inline_formsets.models.Parent'>",
             inlineformset_factory, Parent, Child
         )
@@ -129,7 +136,7 @@ class InlineFormsetFactoryTest(TestCase):
         If we specify fk_name, but it isn't a ForeignKey from the child model
         to the parent model, we should get an exception.
         """
-        self.assertRaises(Exception,
+        self.assertRaisesErrorWithMessage(Exception,
             "fk_name 'school' is not a ForeignKey to <class 'regressiontests.inline_formsets.models.Parent'>",
             inlineformset_factory, Parent, Child, fk_name='school'
         )
@@ -139,7 +146,7 @@ class InlineFormsetFactoryTest(TestCase):
         If the field specified in fk_name is not a ForeignKey, we should get an
         exception.
         """
-        self.assertRaisesRegexp(Exception,
+        self.assertRaisesErrorWithMessage(Exception,
             "<class 'regressiontests.inline_formsets.models.Child'> has no field named 'test'",
             inlineformset_factory, Parent, Child, fk_name='test'
         )

tests/regressiontests/m2m_regress/tests.py
@@ -6,6 +6,13 @@ from models import (SelfRefer, Tag, TagCollection, Entry, SelfReferChild,
 
 
 class M2MRegressionTests(TestCase):
+    def assertRaisesErrorWithMessage(self, error, message, callable, *args, **kwargs):
+        self.assertRaises(error, callable, *args, **kwargs)
+        try:
+            callable(*args, **kwargs)
+        except error, e:
+            self.assertEqual(message, str(e))
+
     def test_multiple_m2m(self):
         # Multiple m2m references to model must be distinguished when
         # accessing the relations through an instance attribute.
@@ -33,8 +40,8 @@ class M2MRegressionTests(TestCase):
         # The secret internal related names for self-referential many-to-many
         # fields shouldn't appear in the list when an error is made.
 
-        self.assertRaisesRegexp(FieldError,
-            "Choices are: id, name, references, related, selfreferchild, selfreferchildsibling$",
+        self.assertRaisesErrorWithMessage(FieldError,
+            "Cannot resolve keyword 'porcupine' into field. Choices are: id, name, references, related, selfreferchild, selfreferchildsibling",
             lambda: SelfRefer.objects.filter(porcupine='fred')
         )
 